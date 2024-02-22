d883508ff3d11d87339b85d4057ec641f078283f
LydianJay <lloydjayedradan@gmail.com>
2024-02-22 10:17:39 +0800

    remove 'result' tab

--- app/Views/header.php
+++ app/Views/header.php
@@ -152,14 +152,7 @@
                         <span class="nav-link-text ms-1">Evaluations</span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white <?php echo ($module_title == 'result') ? 'active' : "" ?>" href="<?php echo site_url('result') ?>">
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="material-icons opacity-10">drive_file_rename_outline</i>
-                        </div>
-                        <span class="nav-link-text ms-1">Result</span>
-                    </a>
-                </li>
+                
                 <li class="nav-item">
                     <a class="nav-link text-white <?php echo ($module_title == 'studenteval') ? 'active' : "" ?>" href="<?php echo site_url('studenteval') ?>">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
